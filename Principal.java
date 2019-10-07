604ccc3d4f40575c8f9a43bc714430bbcfd53053
ArielM24 <ariel_montoya15@outlook.es>
2019-10-06 20:58:23 -0500

    icono

--- Principal.java
+++ Principal.java
@@ -4,51 +4,52 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.geometry.*;
+import javafx.scene.image.*;
+import java.io.*;
+
 
 public class Principal extends Application {
 	private Stage stagePrincipal;
 	private Scene scenePrincipal;
-	private TabPane paneTab;
-	private BorderPane panePrincipal;
-	private GridPane paneTs;
-	private ScrollPane spCuentas;
-	private VBox paneCuentas;
-	private ListView<Cuenta> lvCuentas;
+	private TabPane panePrincipal;
     @Override
 
     public void start(Stage primaryStage){
     	stagePrincipal = primaryStage;
     	initComp();
-    	scenePrincipal = new Scene(paneTab, 1200,700);
+    	scenePrincipal = new Scene(panePrincipal, 1200,700);
     	stagePrincipal.setScene(scenePrincipal);
-    	stagePrincipal.setTitle("Principal");
+    	stagePrincipal.setTitle("Crab");
     	stagePrincipal.show();
     }
 
     private void initComp() {
-    	paneTab = new TabPane();
-    	panePrincipal = new BorderPane();
-    	Tab t1 = new Tab();
-    	t1.setContent(panePrincipal);
-    	paneTab.getTabs().add(t1);
-    	paneCuentas = new VBox(10);
-    	spCuentas = new ScrollPane(paneCuentas);
-    	lvCuentas = new ListView<Cuenta>();
-    	agregaCuentas();
-    	paneCuentas.getChildren().add(lvCuentas);
-    	panePrincipal.setLeft(spCuentas);
-    	panePrincipal.setMargin(spCuentas, new Insets(15));
-
+    	panePrincipal = new TabPane();
+    	Tab t1 = new Tab("Crab");
+    	panePrincipal.getTabs().add(t1);
+    	panePrincipal.getTabs().get(0).setOnCloseRequest(e-> {e.consume();});
+    	Label lblInicial = new Label("CRAB");
+    	VBox panelInicial = new VBox(10,lblInicial);
+    	t1.setContent(panelInicial);
+    	try{
+			InputStream in = getClass().getResourceAsStream("crab.png"); 
+			stagePrincipal.getIcons().add(new Image(in));
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+    	try{
+    		InputStream is = getClass().getResourceAsStream("crab.png");
+    		Image img = new Image(is);
+    		ImageView iv = new ImageView(img);
+    		iv.setFitWidth(500);
+			iv.setFitHeight(500);
+			panelInicial.getChildren().add(iv);
+			panelInicial.setAlignment(Pos.CENTER);
+    	}catch(Exception ex){
+    		ex.printStackTrace();
+    	}
     }
 
-    private void agregaCuentas(){
-    	lvCuentas.getItems().add(new Cuenta("Caja"));
-    	lvCuentas.getItems().add(new Cuenta("Bancos"));
-    	lvCuentas.getItems().add(new Cuenta("Equipo de cómputo"));
-    	lvCuentas.getItems().add(new Cuenta("Equipo de transporte"));
-    	lvCuentas.getItems().add(new Cuenta("Documentos por cobrar"));
-    	lvCuentas.getItems().add(new Cuenta("Gastos de Administración"));
-    }
     public static void main(String args[]) {
     	launch();
     }
---
+++ TabBalanza.java
@@ -0,0 +1,5 @@
+import javafx.scene.control.Tab;
+
+public class TabBalanza extends Tab {
+	
+}
